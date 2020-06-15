44dbfa31fb9ec8a09f82c1b24ffd9115c9bc43ba
Chris Rees <crees@FreeBSD.org>
2020-06-15 16:17:45 +0100

    Select subject and group to view results

--- index.php
+++ index.php
@@ -2,6 +2,21 @@
 namespace TestAnalysis;
 
 require "bin/classes.php";
+
+$allSubjects = Subject::retrieveAll(Subject::NAME);
+
+if (isset($_GET['subject']) && !empty($_GET['subject'])) {
+    $subject = Subject::retrieveByDetail(Subject::ID, $_GET['subject'])[0];
+    $teachingGroups = TeachingGroup::retrieveByDetail(TeachingGroup::SUBJECT_ID, $_GET['subject'], TeachingGroup::NAME);
+    $tests = Test::retrieveByDetail(Test::SUBJECT_ID, $_GET['subject']);
+    
+    if (isset($_GET['teaching_group']) && !empty($_GET['teaching_group'])) {
+        $view = new View($tests, TeachingGroup::retrieveByDetail(TeachingGroup::ID, $_GET['teaching_group'])[0]->getStudents());
+    } else {
+        $view = new View($tests, $subject->getStudents());
+    }
+}
+
 ?>
 <!doctype html>
 <html>
@@ -33,7 +48,46 @@ require "bin/classes.php";
 
 		<h3 class="mb-4"><?= Config::site ?></h3>
 		
-		<pre><?php print_r(Subject::retrieveByDetail('name', "Physics")); ?>
+		<form method="GET">
+    		<div class="form-group row">
+    			<label for="subject" class="col-2 col-form-label">Subject</label>
+      			<div class="col-10">
+        			<select class="form-control" id="subject" name="subject" onchange="this.form.submit()">
+        				<?php
+        				if (!isset($_GET['subject'])) {
+        				    echo "<option value=\"\" selected>Please select subject</option>";
+        				}
+        				foreach ($allSubjects as $s) {
+        				    echo "<option value=\"" . $s->getId() . "\">" . $s->getName() . "</option>";
+        				}
+        				?>
+        			</select>
+        		</div>
+        		<?php if (isset($_GET['subject'])) { ?>
+            		<label for="teaching_group" class="col-2 col-form-label">Teaching group</label>
+            		<div class="col-10">
+            			<select class="form-control" id="teaching_group" name="teaching_group" onchange="this.form.submit()">
+            				<option value="<?= TeachingGroup::OPERATOR_MATCH_ALL ?>">All groups</option>
+            				<?php
+            				foreach ($teachingGroups as $g) {
+            				    if (isset($_GET['teaching_group']) && $_GET['teaching_group'] == $g->getId()) {
+            				        $selected = "selected";
+            				    } else {
+            				        $selected = "";
+            				    }
+            				    echo "<option value=\"" . $g->getId() . "\" $selected>" . $g->getName() . "</option>";
+            				}
+            				?>
+            			</select>
+          			</div>
+          		<?php } /* isset($_GET['subject']) */ ?>
+    		</div>
+		</form>
+		<?php 
+		if (isset($view)) {
+		    $view->print();
+		}
+		?>
 	</div>
 </body>
 
