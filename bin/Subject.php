bda8a8c31e6844544b0022fe2f8254639f35d63b
Chris Rees <crees@FreeBSD.org>
2020-06-15 12:34:48 +0100

    Let's not cache them for now- make it work then make it fast

--- bin/Subject.php
+++ bin/Subject.php
@@ -4,19 +4,22 @@ namespace TestAnalysis;
 class Subject extends DatabaseCollection
 {
     protected $id, $name, $code;
-    protected $teachingGroups;
     
     public function __construct(array $details)
     {
         $this->id = $details['id'];
         $this->name = $details['name'];
         $this->code = $details['code'];
-        $this->teachingGroups = [];
-        foreach (TeachingGroup::retrieveByDetail('Subject', $this->id) as $s) {
-            array_push($this->teachingGroups, $s);
-        }
     }
 
+    public function getTeachingGroups() {
+        return TeachingGroup::retrieveByDetail('Subject_id', $this->id);
+    }
+    
+    public function getTests() {
+        return Test::retrieveByDetail('Subject_id', $this->id);
+    }
+    
     function __destruct()
     {}
 }
