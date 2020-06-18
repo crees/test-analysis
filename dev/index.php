79f4a4b5bc6a4bdecf13b3507c5f3664899a0ac1
Chris Rees <crees@FreeBSD.org>
2020-06-18 11:44:12 +0100

    Introduce test management as well.

--- dev/index.php
+++ dev/index.php
@@ -11,18 +11,22 @@ require "../bin/classes.php";
 		<?php require "../bin/head.php"; ?>
 	</head>
 	<body>
-		<div class="container">
-			<div class="h1">Database tools:</div>
+		<div class="card text-center border-primary mx-auto my-5" style="width: 30rem">
+			<div class="card-body">
+			<h3 class="card-title">Database tools:</h3>
 
-			<div class="row"><a href="arbor_import.php" class="btn btn-primary" role="button">Refresh from Arbor.</a></div>
+			<div class="card-text"><a href="arbor_import.php" class="btn btn-success" role="button">Refresh from Arbor.</a></div>
 
-			<div class="row"><a href="manage_subjects.php" class="btn btn-primary" role="button">Manage subjects and groups.</a></div>
+			<div class="card-text"><a href="manage_subjects.php" class="btn btn-primary" role="button">Manage subjects and groups.</a></div>
+
+			<div class="card-text"><a href="manage_tests.php" class="btn btn-primary" role="button">Manage tests and grade boundaries.</a></div>
 
 			<?php if (isset($_GET['showmethegoodstuff']) && $_GET['showmethegoodstuff'] === "yes") { ?>
-				<div class="row"><a href="database_setup.php" class="btn btn-danger" role="button">Initial database setup- DELETES ALL DATA!</a></div>
+				<div class="card-text"><a href="database_setup.php" class="btn btn-danger" role="button">Initial database setup- DELETES ALL DATA!</a></div>
 			<?php } else { ?>
-				<div class="row"><a href="?showmethegoodstuff=yes" class="btn btn-danger" role="button">Show me the really dangerous stuff.</a></div>
+				<div class="card-text"><a href="?showmethegoodstuff=yes" class="btn btn-warning" role="button">Show me the really dangerous stuff.</a></div>
 			<?php } ?>
+			</div>
 		</div>
 	</body>
 </html>
--- dev/manage_tests.php
+++ dev/manage_tests.php
@@ -4,30 +4,45 @@ namespace TestAnalysis;
 include "../bin/classes.php";
 
 if (isset($_GET['newtest-name'])) {
+    foreach (Test::retrieveAll() as $t) {
+        $tId = $t->getId();
+        if (isset($_GET[Test::SUBJECT_ID . "-$tId"]) && !empty($_GET[Test::SUBJECT_ID . "-$tId"])) {
+            $detail = [];
+            $detail[Test::ID] = $tId;
+            $detail[Test::SUBJECT_ID] = $_GET[Test::SUBJECT_ID . "-$tId"];
+            $detail[Test::NAME] = $_GET[Test::NAME . "-$tId"];
+            // $t->set(Test::TOPIC, $_GET[Test::TOPIC . "-$tId"]);
+            $detail[Test::TOTAL] = $_GET[Test::TOTAL . "-$tId"];
+            if (isset($_GET[Test::CUSTOM_GRADE_BOUNDARIES . "-$tId"])) {
+                $detail[Test::CUSTOM_GRADE_BOUNDARIES] = 1;
+            } else {
+                $detail[Test::CUSTOM_GRADE_BOUNDARIES] = 0;
+            }
+            for ($i = 1; $i <= 9; $i++) {
+                $detail[Test::GRADE . $i] = $_GET[Test::GRADE . $i . "-$tId"];
+            }
+            
+            $newTest = new Test($detail);
+            
+            $newTest->commit();
+        }
+    }
+    
     if (!empty($_GET['newtest-name'])) {
         $newTestDetails = [];
         foreach ($_GET as $k => $v) {
             if (str_contains($k, "newtest-")) {
                 $k = str_replace('newtest-', '', $k);
-                $newTestDetails[$k] = $v;
+                if ($k == Test::CUSTOM_GRADE_BOUNDARIES) {
+                    $newTestDetails[$k] = 1;
+                } else {
+                    $newTestDetails[$k] = $v;
+                }
             }
         }
         $t = new Test($newTestDetails);
         $t->commit();
     }
-    
-    /* TODO This doesn't work yet
-    
-    foreach ($_GET as $k => $v) {
-        if (!empty($v)) {
-            if (str_contains($k, "subject-add-group-")) {
-                $subject = Subject::retrieveByDetail(Subject::ID, str_replace("subject-add-group-", "", $k))[0];
-                $subject->addMember(TeachingGroup::retrieveByDetail(TeachingGroup::ID, $v)[0]);
-            }
-        }
-    }
-    
-    */
 }
 ?>
 <!doctype html>
@@ -57,7 +72,7 @@ $subjects = Subject::retrieveAll(Subject::NAME);
 foreach (Test::retrieveAll(Test::NAME) as $t) {
     $tId = $t->getId();
     // Subject
-    echo "<tr><td><select name=\"subject-$tId\">";
+    echo "<tr><td><select name=\"" . Test::SUBJECT_ID . "-$tId\">";
     foreach ($subjects as $s) {
         if ($s->getId() == $t->get(Test::SUBJECT_ID)) {
             $selected = "selected";
@@ -68,11 +83,11 @@ foreach (Test::retrieveAll(Test::NAME) as $t) {
     }
     echo "</select></td>";    
     // Test name
-    View::makeTextBoxCell("name-$tId", $t->get(Test::NAME));
+    View::makeTextBoxCell(Test::NAME . "-$tId", $t->get(Test::NAME));
     // TODO Topic
     echo "<td>&nbsp;</td>";
     // Total score
-    View::makeTextBoxCell("total-$tId", $t->get(Test::TOTAL));
+    View::makeTextBoxCell(Test::TOTAL . "-$tId", $t->get(Test::TOTAL));
     // Custom grade boundaries?
     if ($t->get(Test::CUSTOM_GRADE_BOUNDARIES)) {
         $checked = "checked";
@@ -80,11 +95,11 @@ foreach (Test::retrieveAll(Test::NAME) as $t) {
         $checked = "";
     }
     echo "<td><div class=\"custom-control custom-checkbox\">";
-    echo "<input type=\"checkbox\" class=\"custom-control-input\" id=\"custom-$tId\" name=\"custom-$tId\" $checked>";
-    echo "<label class=\"custom-control-label\" for=\"custom-$tId\">&nbsp;&nbsp;</label>";
+    echo "<input type=\"checkbox\" class=\"custom-control-input\" id=\"custom-$tId\" name=\"" . Test::CUSTOM_GRADE_BOUNDARIES . "-$tId\" $checked>";
+    echo "<label class=\"custom-control-label\" for=\"custom-$tId\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>";
     echo "</div></td>";
     for ($i = 1; $i <= 9; $i++) {
-        View::makeTextBoxCell("grade-$i-$tId", $t->getGradeBoundary($i));
+        View::makeTextBoxCell("grade$i-$tId", $t->getGradeBoundary($i));
     }
     echo "</tr>";
 }
