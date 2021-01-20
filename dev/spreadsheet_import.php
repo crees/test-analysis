25602065f661df36d5e68c8aa876d7ce298ce93f
Chris Rees <crees@FreeBSD.org>
2021-01-20 15:43:24 +0000

    Adapt for new world order

--- dev/spreadsheet_import.php
+++ dev/spreadsheet_import.php
@@ -8,9 +8,9 @@ die("Comment this out to make it work");
 /**
  * This file takes as input csv in the following format:
  * 
- * Top line contains the IDs of the tests to import in order
+ * Top line contains the IDs of the test components to import in order
  * Subsequent lines:
- * StudentId,firstTestResultA,firstTestResultB,secondTestResultA, [...]
+ * StudentId (from Arbor),firstTestResultA,firstTestResultB,secondTestResultA, [...]
  * 
  * By default it doesn't commit until that line is uncommented.
  * 
@@ -26,36 +26,35 @@ $title = fgetcsv($f);
 
 echo '<pre>';
 
-$tests = [];
+$testComponents = [];
 
 foreach ($title as $t) {
     if (is_numeric($t)) {
-        array_push($tests, $t);
+        array_push($testComponents, $t);
     }
 }
 
-print_r($tests);
+print_r($testComponents);
     
 while (($line = fgetcsv($f)) !== false) {
     $studentId = $line[0];
     $columnIndex = 1;
     $testIndex = 0;
-    while ($columnIndex+1 < sizeof($line) && $testIndex < sizeof($tests)) {
-        $resultA = $line[$columnIndex++];
-        $resultB = $line[$columnIndex++];
-        $testId = $tests[$testIndex++];
-        if (!is_numeric($resultA) || !is_numeric($resultB)) {
+    while ($columnIndex < sizeof($line) && $testIndex < sizeof($testComponents)) {
+        $result = $line[$columnIndex++];
+        $testComponentId = $testComponents[$testIndex++];
+        if (!is_numeric($result)) {
             continue;
         }
-        if ($resultA + $resultB == 0) {
+        if ($result == 0) {
             continue;
         }
-        $result = new TestResult([
-            TestResult::ID => null,
-            TestResult::TEST_ID => $testId,
-            TestResult::STUDENT_ID => $studentId,
-            TestResult::SCORE_A => $resultA,
-            TestResult::SCORE_B => $resultB,
+        $result = new TestComponentResult([
+            TestComponentResult::ID => null,
+            TestComponentResult::TESTCOMPONENT_ID => $testComponentId,
+            TestComponentResult::STUDENT_ID => $studentId,
+            TestComponentResult::SCORE => $result,
+            TestComponentResult::STAFF_ID => $staff->getId(),
         ]);
         print_r($result);
         //$result->commit();
